05ef147e3a85f7597d807d25e8c779f378f9d6ad
Stepanenko1993 <109295820+Stepanenko1993@users.noreply.github.com>
2023-08-02 20:46:17 +0300

    hw-7

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import App from 'components/App';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+    <HashRouter>
+        <App />
+        </HashRouter>
+    </Provider>
+  </React.StrictMode>,
 );
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//       <ToastContainer />
+//       </HashRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
